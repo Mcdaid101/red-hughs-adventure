1c278f69f3c72f6c303f4479bba466ce6fbd4e74
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-01-17 17:41:02 +0000

    added styling to text output

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import time
 import sys
 from time import sleep
+import colorama
+from colorama import Fore
 
 def delay_print(s):
     """
@@ -9,18 +11,20 @@ def delay_print(s):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(0.1)
+        time.sleep(0.05)
 
 
 def get_player_name():
     """
     gets the players name
     """
-    delay_print("Welcome to Red Hugh's adventure\n")
-    delay_print("Enter your name to begin your escape!\n")
+    delay_print(Fore.GREEN + "Welcome to Red Hugh's adventure!\n")
+    delay_print("Enter your name to begin your escape...\n")
     name = input("What is your name prisoner?\n")
-    delay_print("Welcome " + name.capitalize() + " and best of luck!\n")
-    delay_print("L O A D I N G -- G A M E ...")
+    delay_print("Welcome " + name.capitalize() + " and best of luck on your quest!\n")
+    delay_print(Fore.GREEN + "L O A D I N G  G A M E ...")
+    
+
 
 get_player_name()
 
