e8811dfc350e5618bc1fb43793e94bf4d2da1e27
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-01-17 21:11:36 +0000

    added options to the intro scene

--- run.py
+++ run.py
@@ -2,24 +2,28 @@ import time
 import sys
 from time import sleep
 import os
+import re
 
-def cls():
+def clear_screen():
+    """
+    Function clears screen after each scene/function is called.
+    """
     os.system('cls' if os.name=='nt' else 'clear')
 
 def delay_print(s):
     """
-    This function delays text output to a slower speed for style purposes 
+    This function delays text output to a slower speed for style purposes. 
 
     """
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(0.05)
+        time.sleep(0.0)
 
 
 def start_game():
     """
-    gets the players name and begins the game 
+    gets the players name and begins the game. 
     """
     delay_print("Welcome to Red Hugh's adventure!\n")
     delay_print("A text adventure game where you must escape Dublin castle\n")
@@ -30,19 +34,37 @@ def start_game():
     name = user.capitalize()
     delay_print("Welcome " + name + " and best of luck on your quest!\n")
     delay_print("L O A D I N G  G A M E ...\n")
-    cls()
+    clear_screen()
     intro()
     
 
 def intro():
     """
-    The opening scene of the game
+    The opening scene of the game.
     """
-    delay_print("You lie dreaming of your wife and kids, of freedom and a tankard of ale by the fire..\n")
-    delay_print("Until Red Hugh shakes you awake, come on! now's our chance to get out of this dam castle\n")
+    delay_print("You lie dreaming of your wife and kids, of freedom and a tankard of ale by the\n")
+    delay_print("fire..\n")
+    delay_print("Until Red Hugh shakes you awake, come on! now's our chance to get out of this\n")
+    delay_print("dam castle\n")
     delay_print("Red Hugh unlocks your cufflinks with the key he stole from the guard\n")
     delay_print("This is our chance says Red let's go " + name +"!\n")
+    delay_print("The cell door swings open, now how will you escape?\n")
+    clear_screen()
+    print("The watchhouse is straight ahead, the courtyard is on the right and the cells are on our left whispers Red")
+    print("Go: Forward / Right / Left")
+    user_input = input()
+
+    while not re.match("^[forward, right, left]*$", user_input):
+        user_input = input("Please enter either: forward, right or left: ")
+    else:
+        if user_input == "forward":
+            print("You chose " + user_input + " to enter the Watchhouse")
+        elif user_input == "right":
+            print("You chose " + user_input + " to enter the Courtyard")
+        else:
+            print("You chose " + user_input + " to enter the Cells")
     
+     
 
 
 def main():
