312d27597a3e386c1d04c4add667a2589d4fe64c
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-01-17 19:40:56 +0000

    added a clear screen function

--- run.py
+++ run.py
@@ -1,21 +1,25 @@
 import time
 import sys
 from time import sleep
+import os
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 def delay_print(s):
     """
-    This function delays text output to a slower speed
+    This function delays text output to a slower speed for style purposes 
+
     """
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(0.0)
+        time.sleep(0.05)
 
 
 def start_game():
     """
-    gets the players name
+    gets the players name and begins the game 
     """
     delay_print("Welcome to Red Hugh's adventure!\n")
     delay_print("A text adventure game where you must escape Dublin castle\n")
@@ -26,14 +30,18 @@ def start_game():
     name = user.capitalize()
     delay_print("Welcome " + name + " and best of luck on your quest!\n")
     delay_print("L O A D I N G  G A M E ...\n")
+    cls()
     intro()
     
 
 def intro():
+    """
+    The opening scene of the game
+    """
     delay_print("You lie dreaming of your wife and kids, of freedom and a tankard of ale by the fire..\n")
     delay_print("Until Red Hugh shakes you awake, come on! now's our chance to get out of this dam castle\n")
     delay_print("Red Hugh unlocks your cufflinks with the key he stole from the guard\n")
-    delay_print("This is our chance says Red but what's our plan where do we go from here " + name +"?\n")
+    delay_print("This is our chance says Red let's go " + name +"!\n")
     
 
 
